f1c775aa8cf51f3bd03608f96cf51b2deeacf6f1
bharat30156 <66415011+bharat30156@users.noreply.github.com>
2021-01-28 11:39:55 +0000

    implemented plaindrome

---
+++ Algorithms/src/StringsAlgorithm/Palindrome.java
@@ -0,0 +1,76 @@
+package StringsAlgorithm;
+
+/**
+ * 
+ * @author Bharat
+ *
+ */
+public class Palindrome {
+	public static void main(String[] args)
+	{
+		String[] palindromes = {null, "", "aba", "123321"};
+	    for (String s : palindromes) 
+	    {
+	      assert isPalindrome(s) && isPalindromeRecursion(s) && isPalindrome1(s);
+	    }
+
+	    String[] notPalindromes = {"abb", "abc", "abc123"};
+	    for (String s : notPalindromes) 
+	    {
+	      assert !isPalindrome(s) && !isPalindromeRecursion(s) && !isPalindrome1(s);
+	    }
+	}
+	
+	/**
+	 * check if a String is palindrome or not
+	 * 
+	 *  @param s a string to check 
+	 *  @return {@code true} if given String is palindrome otherwise {@code flase}
+	 */
+	public static boolean isPalindrome(String s)
+	{
+		return (s == null || s.length() <= 1 || s.equals(new StringBuilder(s).reverse().toString()));
+	}
+	
+	/**
+	 * check if String is palindrome String or not using recursion 
+	 * 
+	 * @param s a String to check
+	 * @return {@code true} if given string is palindrome, otherwise{@code false}
+	 */
+	public static boolean isPalindromeRecursion(String s)
+	{
+		if(s == null || s.length() <= 1)
+		{
+			return true;
+		}
+		
+		if(s.charAt(0) != s.charAt(s.length() - 1))
+		{
+			return false;
+		}
+		
+		return isPalindrome(s.substring(1, s.length() - 1));
+	}
+	
+	/**
+	 * check if a string is palindrome string or not another way
+	 * 
+	 * @param s a string to check
+	 * @return {@code true} if a given string is palindrome, otherwise {@code false}
+	 */
+	
+	public static boolean isPalindrome1(String s)
+	{
+		if(s == null || s.length() <= 1)
+			return true;
+		for (int i = 0, j = s.length() - 1; i < j; ++i, --j) 
+		{
+			if (s.charAt(i) != s.charAt(j)) 
+			{
+		        return false;
+		    }
+		}
+		    return true;
+	}
+}
