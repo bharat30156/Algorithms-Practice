76208023c6452df38950e7931ba2fcff44f8ed01
bharat30156 <bk30156@gmail.com>
2021-03-26 10:55:20 +0000

    Implemented Factorial Recursion

---
+++ Algorithms/src/Maths/FactorialRecursion.java
@@ -0,0 +1,28 @@
+package Maths;
+
+public class FactorialRecursion {
+	public static void main(String[] args)
+	{
+		assert factorial(0) == 1;
+		assert factorial(1) == 1;
+		assert factorial(2) == 2;
+		assert factorial(3) == 6;
+		assert factorial(5) == 120;
+		
+	}
+	/**
+	 * Recursive FcatorialRecxursion method
+	 * 
+	 * @param n the number to factorial
+	 * @return The factorial of the number
+	 */
+	public static long factorial(int n)
+	{
+		if(n < 0)
+		{
+			throw new IllegalArgumentException("number is negative");
+		}
+		return n == 0 || n == 1 ? 1 : n * factorial(n -1);
+	}
+
+}
